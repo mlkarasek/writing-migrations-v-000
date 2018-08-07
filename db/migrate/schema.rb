d753df9795898978bcb291fa6e518414073275f1
Monica  Karasek <mlkarasek@gmail.com>
2018-08-07 22:16:49 +0000

    automatically backed up by learn

--- db/migrate/schema.rb
+++
@@ -1,9 +0,0 @@
-ActiveRecord::Schema.define(version: 3) do
-
-   create_table "students", force: :cascade do |t|
-     t.string "name"
-     t.integer "grade"
-     t.datetime "birthdate"
-   end
-
- end
